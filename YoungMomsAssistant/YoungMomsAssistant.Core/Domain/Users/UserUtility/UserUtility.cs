5e8d8b1ad92d9c62c5b11daa03e25181c862e876
ma.yurchin <ma.yurchin@andersenlab.com>
2019-04-04 15:48:52 +0300

    Add Utilities

---
+++ YoungMomsAssistant/YoungMomsAssistant.Core/Domain/Users/UserUtility/UserUtility.cs
@@ -0,0 +1,8 @@
+﻿namespace YoungMomsAssistant.Core.Domain.Users.Utilities {
+    static class UserUtility {
+
+        public static string GetPasswordHash(string password) {
+            return Utility.GetSHA256Hash(Utility.GetSHA256Hash(password));
+        } 
+    }
+}
