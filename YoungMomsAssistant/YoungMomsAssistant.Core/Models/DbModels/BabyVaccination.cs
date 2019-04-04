9ddc3ba86afff32c7f00a8498d4603cfdd8f79f7
HyperactivePanda <hyperactivepanda.2000@gmail.com>
2019-04-04 23:12:42 +0300

    [ref #5] Add diseases and vaccinations models

---
+++ YoungMomsAssistant/YoungMomsAssistant.Core/Models/DbModels/BabyVaccination.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace YoungMomsAssistant.Core.Models.DbModels {
+    public class BabyVaccination {
+        public int Id { get; set; }
+
+        [ForeignKey("Baby")]
+        public int Baby_Id { get; set; }
+        public Baby Baby { get; set; }
+
+        [ForeignKey("Vaccination")]
+        public int Vaccination_Id { get; set; }
+        public Vaccination Vaccination { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
