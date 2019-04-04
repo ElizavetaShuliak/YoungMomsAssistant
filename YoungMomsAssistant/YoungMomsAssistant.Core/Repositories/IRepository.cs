1591e46fc1649927a9da117c5af1b187d5bf82a5
ma.yurchin <ma.yurchin@andersenlab.com>
2019-04-04 15:02:45 +0300

    Add generic repository interface and UserRepository

---
+++ YoungMomsAssistant/YoungMomsAssistant.Core/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace YoungMomsAssistant.Core.Repositories {
+    public interface IRepository<TModel> {
+        Task<TModel> GetAsync(int id);
+
+        Task<List<TModel>> GetAsync();
+
+        Task AddAsync(TModel model);
+
+        Task UpdateAsync(TModel model);
+
+        Task RemoveAsync(int id);
+    }
+}
