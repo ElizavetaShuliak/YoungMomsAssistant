c38743215a3ebaf7668874981f00fa22c95be8f0
HyperactivePanda <hyperactivepanda.2000@gmail.com>
2019-04-07 20:19:26 +0300

    [ref #11] Add BabyManager

---
+++ YoungMomsAssistant/YoungMomsAssistant.Core/Domain/Babies/BabyManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Tasks;
+using YoungMomsAssistant.Core.Models.DbModels;
+using YoungMomsAssistant.Core.Models.DtoModels;
+using YoungMomsAssistant.Core.Repositories;
+
+namespace YoungMomsAssistant.Core.Domain.Babies {
+    public class BabyManager {
+
+        private IRepository<User> _userInfoRepo;
+        private IRepository<Baby> _babyRepo;
+        private IRepository<BabyInfo> _babyInfoRepo;
+    
+        public BabyManager(
+            IRepository<User> userRepository,
+            IRepository<Baby> babyRepository,
+            IRepository<BabyInfo> babyInfoRepository) {
+            _userInfoRepo = userRepository;
+            _babyRepo = babyRepository;
+            _babyInfoRepo = babyInfoRepository;
+        }
+
+        public async Task AddNewBabyAsync(BabyDto babyDto, UserDto userDto) {
+            var userDb = await _userInfoRepo
+                .FindAsync(user => user.Email == user.Email);
+
+            if (userDb == null) {
+                return;
+            }
+
+            var baby = new Baby {
+                FirstName = babyDto.FirstName,
+                LastName = babyDto.LastName,
+                BirthDay = babyDto.BirthDay
+            };
+
+            await _babyRepo.AddAsync(baby);
+
+            var babyInfo = new BabyInfo {
+                Baby = baby,
+                BloodType = babyDto.BloodType,
+                CurrentWeight = babyDto.CurrentWeight,
+                CurrentGrowth = babyDto.CurrentGrowth,
+                Sex = babyDto.Sex
+            };
+
+            await _babyInfoRepo.AddAsync(babyInfo);
+        }
+    }
+}
---
+++ YoungMomsAssistant/YoungMomsAssistant.Core/Models/DtoModels/BabyDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace YoungMomsAssistant.Core.Models.DtoModels {
+    public class BabyDto {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime BirthDay { get; set; }
+
+        public string Sex { get; set; }
+
+        public string BloodType { get; set; }
+
+        public float CurrentGrowth { get; set; }
+
+        public float CurrentWeight { get; set; }
+    }
+}
